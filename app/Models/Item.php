e8a3426cdff326a6bc2e3d7adabf020e9513416f
Vyacheslav Bazhenov <vyacheslav.bazhenov@nutnet.ru>
2020-05-29 18:18:18 +0400

    Format response

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Item extends Model
 {
     protected $fillable = ['title', 'is_sales', 'description', 'image_url', 'price', 'count'];
+
+    public function getIsSalesAttribute($value) {
+        return (boolean) $value;
+    }
 }
